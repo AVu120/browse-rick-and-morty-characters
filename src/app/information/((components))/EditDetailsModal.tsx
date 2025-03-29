4249787e1674f89703df3d89018b3e413b069e4d
Anthony Hien Vu <avu120@gmail.com>
2025-03-29 19:27:13 +1100

    move state of edit details modal into separate component to improve performance of typing into details modal form.

---
+++ src/app/information/((components))/EditDetailsModal.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import {
+  Button,
+  CloseButton,
+  Dialog,
+  Field,
+  Input,
+  Portal,
+} from "@chakra-ui/react";
+import { useState, ReactNode, FormEvent } from "react";
+
+interface EditDetailsModalProps {
+  username: string;
+  jobTitle: string;
+  updateDetails: (username: string, jobTitle: string) => void;
+  children: ReactNode;
+}
+
+export default function EditDetailsModal({
+  username,
+  jobTitle,
+  updateDetails,
+  children,
+}: EditDetailsModalProps) {
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [currentUsername, setCurrentUsername] = useState(username);
+  const [currentJobTitle, setCurrentJobTitle] = useState(jobTitle);
+
+  const isDisabled = !currentUsername || !currentJobTitle;
+
+  const changeCurrentUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentUsername(e.target.value);
+  };
+
+  const changeCurrentJobTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentJobTitle(e.target.value);
+  };
+
+  const onClose = () => {
+    setCurrentUsername(username);
+    setCurrentJobTitle(jobTitle);
+    setIsEditModalOpen(false);
+  };
+
+  const onSave = (e: FormEvent) => {
+    e.preventDefault();
+    if (!currentUsername || !currentJobTitle) {
+      return alert("Please fill out all fields");
+    }
+
+    updateDetails(currentUsername, currentJobTitle);
+    setIsEditModalOpen(false);
+  };
+
+  return (
+    <Dialog.Root
+      size="full"
+      open={isEditModalOpen}
+      onOpenChange={(e) => setIsEditModalOpen(e.open)}
+    >
+      <Dialog.Trigger asChild>{children}</Dialog.Trigger>
+      <Portal>
+        <Dialog.Backdrop />
+        <Dialog.Positioner>
+          <Dialog.Content>
+            <Dialog.Header>
+              <Dialog.Title>Update your details</Dialog.Title>
+            </Dialog.Header>
+            <form onSubmit={onSave}>
+              <Dialog.Body className="flex flex-col gap-4">
+                <Field.Root invalid={!currentUsername}>
+                  <Field.Label>Username</Field.Label>
+                  <Input
+                    onChange={changeCurrentUsername}
+                    value={currentUsername}
+                    placeholder="Enter your username"
+                  />
+                  <Field.ErrorText>This field is required</Field.ErrorText>
+                </Field.Root>
+                <Field.Root invalid={!currentJobTitle}>
+                  <Field.Label>Job Title</Field.Label>
+                  <Input
+                    onChange={changeCurrentJobTitle}
+                    value={currentJobTitle}
+                    placeholder="Enter your job title"
+                  />
+                  <Field.ErrorText>This field is required</Field.ErrorText>
+                </Field.Root>
+              </Dialog.Body>
+              <Dialog.Footer>
+                <Dialog.ActionTrigger asChild>
+                  <Button variant="outline" onClick={onClose}>
+                    Cancel
+                  </Button>
+                </Dialog.ActionTrigger>
+                <Button disabled={isDisabled} type="submit">
+                  Save
+                </Button>
+              </Dialog.Footer>
+              <Dialog.CloseTrigger asChild>
+                <CloseButton size="sm" onClick={onClose} />
+              </Dialog.CloseTrigger>
+            </form>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root>
+  );
+}
--- src/app/information/page.tsx
+++ src/app/information/page.tsx
@@ -1,17 +1,6 @@
 "use client";
 
-import {
-  Box,
-  Button,
-  CloseButton,
-  Dialog,
-  Field,
-  Flex,
-  IconButton,
-  Input,
-  Portal,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Flex, IconButton, Text } from "@chakra-ui/react";
 import { Suspense, useEffect, useState } from "react";
 import { LuUser } from "react-icons/lu";
 import { useDetails } from "~/hooks/useDetails";
@@ -20,6 +9,7 @@ import { apolloClient } from "~/lib/apollo-client";
 import { CardHorizontal } from "~/components/ui/Card";
 import { PaginationButtons } from "~/components/ui/PaginationButtons";
 import { useRouter, useSearchParams } from "next/navigation";
+import EditDetailsModal from "./((components))/editDetailsModal";
 
 interface Character {
   name: string;
@@ -69,9 +59,6 @@ function InformationContent() {
   const pageNumber = parseInt(pageQueryParam || "");
 
   const { username, jobTitle, hasLoaded, updateDetails } = useDetails();
-  const [currentUsername, setCurrentUsername] = useState("");
-  const [currentJobTitle, setCurrentJobTitle] = useState(jobTitle);
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [characters, setCharacters] = useState<Character[]>([]);
   const [isFetching, setIsFetching] = useState(false);
 
@@ -82,18 +69,7 @@ function InformationContent() {
     prev: 0,
   });
 
-  const changeCurrentUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrentUsername(e.target.value);
-  };
-
-  const changeCurrentJobTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrentJobTitle(e.target.value);
-  };
-
-  const isDisabled = !username || !jobTitle;
-
   const fetchCharacters = (page: number) => {
-    console.log("Fetching characters...");
     // Doing call here instead of using useQuery because I don't want to run this on component mount.
     // I only want to run this when I need to (e.g. event-driven) such as when both username and jobTitle are entered.
     setIsFetching(true);
@@ -127,42 +103,21 @@ function InformationContent() {
   };
 
   useEffect(() => {
-    setCurrentUsername(username);
-    setCurrentJobTitle(jobTitle);
-  }, [username, jobTitle]);
-
-  const updatePageNumber = (page: number) => {
-    const params = new URLSearchParams(searchParams);
-    params.set("page", page.toString());
-    router.replace(`?${params.toString()}`); // this won't reload the page
-  };
-
-  useEffect(() => {
+    // Only retrieve the graphql data on load if both username, jobTitle and queryParam are entered.
     if (!!pageNumber) {
-      // Only retrieve the graphql data on load if both username, jobTitle and queryParam are entered.
       if (!!username && !!jobTitle) {
         fetchCharacters(pageNumber);
       }
-      // If pageQueryParam is not present, set it to 1.
     } else {
       updatePageNumber(DEFAULT_PAGE_NUMBER);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [username, jobTitle, pageNumber]);
 
-  const onClose = () => {
-    setCurrentUsername(username);
-    setCurrentJobTitle(jobTitle);
-  };
-
-  const onSave = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (!currentUsername || !currentJobTitle) {
-      return alert("Please fill out all fields");
-    }
-
-    updateDetails(currentUsername, currentJobTitle);
-    setIsEditModalOpen(false);
+  const updatePageNumber = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.replace(`?${params.toString()}`); // this won't reload the page
   };
 
   return (
@@ -182,62 +137,15 @@ function InformationContent() {
             : "Loading..."}
         </Text>
 
-        <Dialog.Root
-          size="full"
-          open={isEditModalOpen}
-          onOpenChange={(e) => setIsEditModalOpen(e.open)}
+        <EditDetailsModal
+          username={username}
+          jobTitle={jobTitle}
+          updateDetails={updateDetails}
         >
-          <Dialog.Trigger asChild>
-            <IconButton aria-label="Update user details" rounded="full">
-              <LuUser />
-            </IconButton>
-          </Dialog.Trigger>
-          <Portal>
-            <Dialog.Backdrop />
-            <Dialog.Positioner>
-              <Dialog.Content>
-                <Dialog.Header>
-                  <Dialog.Title>Update your details</Dialog.Title>
-                </Dialog.Header>
-                <form onSubmit={onSave}>
-                  <Dialog.Body className="flex flex-col gap-4">
-                    <Field.Root invalid={!currentUsername}>
-                      <Field.Label>Username</Field.Label>
-                      <Input
-                        onChange={changeCurrentUsername}
-                        value={currentUsername}
-                        placeholder="Enter your username"
-                      />
-                      <Field.ErrorText>This field is required</Field.ErrorText>
-                    </Field.Root>
-                    <Field.Root invalid={!currentJobTitle}>
-                      <Field.Label>Job Title</Field.Label>
-                      <Input
-                        onChange={changeCurrentJobTitle}
-                        value={currentJobTitle}
-                        placeholder="Enter your job title"
-                      />
-                      <Field.ErrorText>This field is required</Field.ErrorText>
-                    </Field.Root>
-                  </Dialog.Body>
-                  <Dialog.Footer>
-                    <Dialog.ActionTrigger asChild>
-                      <Button variant="outline" onClick={onClose}>
-                        Cancel
-                      </Button>
-                    </Dialog.ActionTrigger>
-                    <Button disabled={isDisabled} type="submit">
-                      Save
-                    </Button>
-                  </Dialog.Footer>
-                  <Dialog.CloseTrigger asChild>
-                    <CloseButton size="sm" onClick={onClose} />
-                  </Dialog.CloseTrigger>
-                </form>
-              </Dialog.Content>
-            </Dialog.Positioner>
-          </Portal>
-        </Dialog.Root>
+          <IconButton aria-label="Update user details" rounded="full">
+            <LuUser />
+          </IconButton>
+        </EditDetailsModal>
       </Flex>
       <Box className="flex flex-col gap-4 items-center justify-start flex-grow">
         <PaginationButtons
